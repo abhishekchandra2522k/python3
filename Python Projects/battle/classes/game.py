7710d05e26cd2d1b85293bb72a74fbb70df96b85
abhishekchandra2522k <1ac23456789@gmail.com>
2020-11-23 21:32:21 +0530

    Modify game.py

--- Python Projects/battle/classes/game.py
+++ Python Projects/battle/classes/game.py
@@ -24,3 +24,49 @@ class Person:
     
     def generate_damage(self):
         return random.randrange(self.atkl, self.atkh)
+    
+    def generate_spell_damage(self, i):
+        mgl = self.magic[i]['dmg'] - 5
+        mgh = self.magic[i]['dmg'] + 5
+        return random.randrange(mgl, mgh)
+    
+    def take_damage(self, dmg):
+            self.hp -= dmg
+            if self.hp < 0:
+                self.hp = 0
+            return self.hp
+    
+    def get_hp(self):
+        return self.hp
+    
+    def get_maxhp(self):
+        return self.maxhp
+
+    def get_mp(self):
+        return self.mp
+    
+    def get_maxmp(self):
+        return self.maxmp
+    
+    def reduce_mp(self, cost):
+        self.mp -= cost
+    
+    def generate_spell_name(self,i):
+        return self.magic[i]['Name']
+
+    def get_spell_mp_cost(self, i):
+        return self.magic[i]['cost']
+    
+    def choose_action(self):
+        i = 1
+        print('Actions')
+        for item in self.actions:
+            print(str(i), :, item)
+            i += 1
+    
+    def choose_magic(self):
+        i = 1
+        print('Magic')
+        for spell in self.magic:
+            print(str(i), :, magic[i]["Name"], "(cost:", str(spell['mp']) + ")")
+            i += 1
