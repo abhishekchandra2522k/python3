54d628b9ee5912c050bf16d2c176316f11adaadb
abhishekchandra2522k <1ac23456789@gmail.com>
2020-11-23 21:32:43 +0530

    Add main.py

---
+++ Python Projects/battle/main.py
@@ -0,0 +1,8 @@
+from classes.game import Person, bcolors
+
+magic = [{'Name' : 'Fire', 'cost' : 10, 'dmg' : 60},
+         {'Name' : 'Thunder', 'cost' : 10, 'dmg' : 80},
+         {'Name' : 'Bllizard', 'cost' : 10, 'dmg' : 60}]
+
+player = Person(460, 65, 60, 34, magic)
+
