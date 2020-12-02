ae464cb3a948cee10c2cebdbc52a30b8a594c930
abhishekchandra2522k <1ac23456789@gmail.com>
2020-12-02 18:54:48 +0530

    Modify game.py, modify items function

--- Python Projects/battle/classes/game.py
+++ Python Projects/battle/classes/game.py
@@ -73,5 +73,5 @@ class Person:
         
         print("\n" + bcolors.OKBLUE + bcolors.BOLD + "ITEMS : " + bcolors.ENDC)
         for item in self.items:
-            print("     " + str(i) + ". " + item.name + " : " + item.description + " (x5) ")
+            print("     " + str(i) + ". " + item["item"].name + " : " + item["item"].description + "(x" + str(item["quantity"]) + ")")
             i += 1
