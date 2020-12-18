53aace4e06665fbaff02a2ca45da40c53b540b6e
abhishekchandra2522k <1ac23456789@gmail.com>
2020-12-18 20:01:39 +0530

    Modift requeststest2.py

--- requeststest_2.py
+++ requeststest_2.py
@@ -2,4 +2,17 @@ import requests
 from io import BytesIO
 from PIL import Image
 
-r = requests.get()
+r = requests.get("https://wallpaperaccess.com/full/630848.jpg")
+
+print("Status code", r.status_code)
+
+image = Image.open(BytesIO(r.content)) #binary data
+
+print(image.size, image.format, image.mode)
+
+path = "./image." + image.format
+
+try:
+    image.save(path, image.format)
+except IOError:
+    print("Cannot save image")
