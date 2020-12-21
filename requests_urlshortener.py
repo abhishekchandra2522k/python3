786d7800e928441d72f44a851ead8ceb65a47feb
abhishekchandra2522k <1ac23456789@gmail.com>
2020-12-21 22:12:55 +0530

    Add requests_urlshotener.py

---
+++ requests_urlshortener.py
@@ -0,0 +1,11 @@
+import requests
+import simplejson as json
+
+url = "https://www.googleapis.com/urlshortener/v1/url"
+
+payload = {'longurl' : "http://example.com"}
+headers = {"Content-type" : "application/json"}
+
+r = requests.post(url, json=payload, headers = headers)
+
+print(r.text)
