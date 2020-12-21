e36ae428b3e6ea80c7bb5cba0cf6bee0a25ecd7a
abhishekchandra2522k <1ac23456789@gmail.com>
2020-12-21 22:12:13 +0530

    Modify requestspost.py

--- requestspost.py
+++ requestspost.py
@@ -2,8 +2,8 @@ import requests
 
 my_data = {'name' : 'Abhishek', 'email': "ab25chandra@gmail.com"}
 
-r = requests.post('form.html', data=my_data)
+r = requests.post('https://tryphp.w3schools.com/showphp.php?filename=demo_form_post', data=my_data)
 
-f = open("./requests_form.html", "w+")
+f = open("requests_form.html", "w+")
 
-f.write()
+f.write(r.text)
