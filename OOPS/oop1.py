4fe02887400268a61f6f7c0cf8db335b65a04b2d
abhishekchandra2522k <1ac23456789@gmail.com>
2021-04-11 10:52:15 +0530

    Updtae oop1.py

--- OOPS/oop1.py
+++ OOPS/oop1.py
@@ -5,11 +5,11 @@ se2 = ["Software Engineer", "Siddharth", 24, "Senior", 1000000]
 # class is only a blueprint of data structure
 class SoftwareEngineer():
 
-    #class attribute
+    #class attribute (same for every instance/object)
     alias = "Keyboard Magician"
 
     def __init__(self, name, age, level, salary):
-        #instance attributes
+        #instance attributes (only belongs to one object/instance that we created and not to the whole class)
         self.name = name
         self.age = age
         self.level = level
@@ -20,3 +20,9 @@ se1 = SoftwareEngineer("Abhishek", 21, "Junior", 600000)
 
 print(se1.name, se1.age)
 print(SoftwareEngineer.alias)
+print(se1.alias) # this also works, instance calling class attribute
+
+se2 = SoftwareEngineer("Siddharth", 24, "Senior", 1000000)
+print(se2.alias)
+
+# Recap
