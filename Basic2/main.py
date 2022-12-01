60f4d3d25cd1a629f6207b00a7aac43ec79eb1b7
abhishekchandra2522k <1ac23456789@gmail.com>
2022-12-02 00:23:45 +0530

    Update basic2

--- Basic2/main.py
+++ Basic2/main.py
@@ -1,10 +1,23 @@
 calculation_to_units = 24
 name_of_unit = "hours"
+# scn = int(input(""))
 
 
-def days_to_units(days, custom_message):
-    print(f"{days} days are {days * calculation_to_units} {name_of_unit}")
-    print(custom_message)
+def days_to_units(days):
+    return f"{days} days are {days * calculation_to_units} {name_of_unit}"
 
 
-days_to_units(3, "greeting")
+def validate_and_execute():
+    try:
+        user_input_num = int(user_input)
+        if user_input_num > 0:
+            calculated_value = days_to_units(user_input_num)
+            print(calculated_value)
+        else:
+            print("invalid number")
+    except ValueError:
+        print("Invalid value entered")
+
+
+user_input = input("Enter a value: \n")
+validate_and_execute()
