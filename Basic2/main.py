a893e22427c0735397ab37cbf0196dd0a176eb79
abhishekchandra2522k <1ac23456789@gmail.com>
2022-12-05 23:51:04 +0530

    Update basic2, learned comments, while, for loop, split(), and lists

--- Basic2/main.py
+++ Basic2/main.py
@@ -9,7 +9,9 @@ def days_to_units(days):
 
 def validate_and_execute():
     try:
-        user_input_num = int(user_input)
+        user_input_num = int(num_of_days)
+
+        # we want to do conversion only for positive integers
         if user_input_num > 0:
             calculated_value = days_to_units(user_input_num)
             print(calculated_value)
@@ -19,5 +21,10 @@ def validate_and_execute():
         print("Invalid value entered")
 
 
-user_input = input("Enter a value: \n")
-validate_and_execute()
+user_input = ''
+while user_input != "exit":
+    user_input = input("Enter number of days as a list : \n")
+    print(type(user_input.split(",")))
+    print(user_input.split(","))
+    for num_of_days in user_input.split(","):
+        validate_and_execute()
