7b7f895f3e249ee2892de7cd2de0d46110dfa213
abhishekchandra2522k <1ac23456789@gmail.com>
2022-12-12 22:30:41 +0530

    Update countdown-app

--- Small Projects/countdown-app/time_till_deadline.py
+++ Small Projects/countdown-app/time_till_deadline.py
@@ -1,1 +1,17 @@
-import datetime
+from datetime import datetime
+
+user_in = input("enter your goal with a deadline separated by colon\n")
+input_list = user_in.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
+
+deadline_date = datetime.strptime(deadline, "%d.%m.%Y")
+today_date = datetime.today()
+
+# calculate how many days from now till deadline
+time_till = deadline_date - today_date
+
+print(f"Dear user! Time remaining for your goal ({goal}): {time_till.days} days")
+hours_till = int(time_till.total_seconds() / 60 / 60)
+print(f"Dear user! Time remaining for your goal ({goal}): {hours_till} hours")
