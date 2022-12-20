e113f867c4333d4ed93d1fc8147056ec3d591804
abhishekchandra2522k <1ac23456789@gmail.com>
2022-12-20 22:05:09 +0530

    Add oops

---
+++ OOPS/oops/user.py
@@ -0,0 +1,12 @@
+class User:
+    def __init__(self, email, name, password, current_job_title):
+        self.email = email
+        self.name = name
+        self.password = password
+        self.current_job_title = current_job_title
+
+    def change_password(self, new_password):
+        self.password = new_password
+
+    def change_job_title(self, new_job_title):
+        self.current_job_title = new_job_title
